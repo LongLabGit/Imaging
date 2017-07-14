63fe6b460b60bec6a1968bac3e016a5fff9e098d
vigi_katlowitz <akatlowitz@gmail.com>
2017-07-14 12:22:53 -0400

    Removed old files
    no need for backups

--- OldCode/checksum.m
+++
@@ -1,7 +0,0 @@
-folder='Data\105\Planes\';
-Opt.Input='file';
-flds=dir('A:\Michel\Michel2photon\ImagingAnalysis\Data\105\Planes');
-for i=15:length(flds)
-    f=flds{i};
-    Hash{i} = DataHash([folder,f,'\ROIs\RoiSet.zip'], Opt);
-end
--- OldCode/plotAudioStuff.m
+++
@@ -1,5 +0,0 @@
-[a,fs]=audioread(Motif(3).audioF);
-t=linspace(Motif(3).audioTimesWARP(1),Motif(3).audioTimesWARP(2),length(a));
-plot(t,a);
-axis tight;
-line(InitialC(4).bursts+[-2,2]*InitialC(4).Sburst,[1,1]*.1)
